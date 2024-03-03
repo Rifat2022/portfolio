4f1f9bb9caf1e280f0bb111478b5159e9bfe54ab
Md.Rifat Hossain <rifat@siderian.com>
2024-03-03 11:25:50 +0600

    routing initialized

--- portfolio-ng-app/src/app/app.routes.ts
+++ portfolio-ng-app/src/app/app.routes.ts
@@ -7,21 +7,32 @@ import { BlogComponent } from './home/blog/blog.component';
 import { ContactComponent } from './home/contact/contact.component';
 import { MyServiceComponent } from './home/my-service/my-service.component';
 import { WorkComponent } from './home/work/work.component';
+import { HeroComponent } from './home/hero/hero.component';
 
 export const routes: Routes = [
-    { 
-        path: 'home',
-        title: 'Home Component', 
-        component:HomeComponent, 
-        children: [
-            { path: "about", title:"About", component:AboutComponent }, 
-            { path: "achievement", title:"Achievement", component:AchievementComponent }, 
-            { path: "blog", title:"Blog", component:BlogComponent }, 
-            { path: "contact", title:"Contact", component:ContactComponent },
-            { path: "services", title:"My Services", component:MyServiceComponent }, 
-            { path: "work", title:"Work", component:WorkComponent },
-        ]
-    },
-    { path: '', redirectTo:'/home', pathMatch: 'full' },
-    { path: '**', component:NotFoundPageComponent }
+        { 
+            path: '',
+            redirectTo: '/home',
+            pathMatch: 'full'
+        },
+        { 
+            path: 'home',
+            title: 'Home ',
+            component: HomeComponent,
+            children: [
+                { path: '', redirectTo: 'hero', pathMatch: 'full' },
+                { path: 'hero', title: "Hero", component: HeroComponent },
+                { path: 'about', title: "About", component: AboutComponent },
+                { path: 'achievement', title: "Achievement", component: AchievementComponent },
+                { path: 'blog', title: "Blog", component: BlogComponent },
+                { path: 'contact', title: "Contact", component: ContactComponent },
+                { path: 'services', title: "Services", component: MyServiceComponent },
+                { path: 'work',title: "Work", component: WorkComponent },
+            ]
+        },
+        { 
+            path: '**',
+            component: NotFoundPageComponent
+        }
+    
 ];
--- portfolio-ng-app/src/app/home/home.component.ts
+++ portfolio-ng-app/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
